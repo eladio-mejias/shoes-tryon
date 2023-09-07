1a31d0705a5d32c5061e477f9b948a58d78d7b2f
ej <eladio.mejias@paveca.com.ve>
2023-09-07 15:00:02 -0400

    Added Nike

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const _settings = {
   
     // CONVERSES SHOES:
     // 3D models:
-    shoeRightPath: 'assets/vans2.glb',
+    shoeRightPath: 'assets/nike.glb',
     isModelLightMapped: true,
     occluderPath: 'assets/occluder.glb',
   
