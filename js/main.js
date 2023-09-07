fa07b7b31bfec419866755201b0e24337a48dd92
ej <eladio.mejias@paveca.com.ve>
2023-09-07 12:20:31 -0400

    Update shoes

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const _settings = {
   
     // CONVERSES SHOES:
     // 3D models:
-    shoeRightPath: 'assets/vansShoe.glb',
+    shoeRightPath: 'assets/converseShoe2.glb',
     isModelLightMapped: true,
     occluderPath: 'assets/occluder.glb',
   
