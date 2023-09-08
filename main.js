2fa54225163b4da7cc878a140b9723ffa3b10d06
ej <eladio.mejias@paveca.com.ve>
2023-09-08 16:25:00 -0400

    Update with newer model

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const _settings = {
   
     // CONVERSES SHOES:
     // 3D models:
-    shoeRightPath: 'assets/nike-future.glb',
+    shoeRightPath: 'assets/nike-future-2.glb',
     isModelLightMapped: false,
     occluderPath: 'assets/occluder.glb',
   
